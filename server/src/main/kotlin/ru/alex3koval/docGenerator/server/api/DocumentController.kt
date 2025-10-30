3bdb7da20369c5e4c2f6ba54664e1e0bd6914bdf
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-30 19:37:19 +0300

    master: changes in controller.

--- server/src/main/kotlin/ru/alex3koval/docGenerator/server/api/DocumentController.kt
+++ server/src/main/kotlin/ru/alex3koval/docGenerator/server/api/DocumentController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 import ru.alex3koval.docGenerator.appImpl.command.factory.CreateDocumentCommandFactory
 import ru.alex3koval.docGenerator.domain.command.CreateDocumentCommand
 import ru.alex3koval.docGenerator.server.api.dto.request.CreateDocumentRequest
@@ -14,18 +15,19 @@ import ru.alex3koval.docGenerator.server.api.dto.request.CreateDocumentRequest
 @RestController
 @RequestMapping("document")
 class DocumentController(
-    private val createDocumentCommandFactory: CreateDocumentCommandFactory<*, *, *>
+    private val createDocumentCommandFactory: CreateDocumentCommandFactory<*, *, Any>
 ) {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun add(
-        @Validated @RequestBody body: CreateDocumentRequest
-    ): Int = createDocumentCommandFactory
+        @Validated @RequestBody body: CreateDocumentRequest<*>
+    ): Mono<*> = createDocumentCommandFactory
         .create(
             dto = CreateDocumentCommand.DTO(
-
+                templateId = body.templateId,
+                rawModel = mapOf("hello" to "world"),
+                format = body.format
             )
         )
         .execute()
-        .getOrThrow()
 }
