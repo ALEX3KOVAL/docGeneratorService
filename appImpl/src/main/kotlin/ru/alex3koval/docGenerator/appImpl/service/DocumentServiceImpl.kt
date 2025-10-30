b1913d746ba8487fb7e6ab6a0ed313099800432c
Alexandr Kovalenko <destructsymp@gmail.com>
2025-10-30 19:37:34 +0300

    master: doc service.

--- appImpl/src/main/kotlin/ru/alex3koval/docGenerator/appImpl/service/DocumentServiceImpl.kt
+++ appImpl/src/main/kotlin/ru/alex3koval/docGenerator/appImpl/service/DocumentServiceImpl.kt
@@ -3,35 +3,92 @@ package ru.alex3koval.docGenerator.appImpl.service
 import com.fasterxml.jackson.databind.ObjectMapper
 import reactor.core.publisher.Mono
 import ru.alex3koval.docGenerator.domain.contract.BaseGeneratedDocDTO
+import ru.alex3koval.docGenerator.domain.entity.DocumentEntity
+import ru.alex3koval.docGenerator.domain.entity.DocumentTemplateEntity
 import ru.alex3koval.docGenerator.domain.repository.document.DocumentRepository
+import ru.alex3koval.docGenerator.domain.repository.document.DocumentTemplateRepository
+import ru.alex3koval.docGenerator.domain.repository.document.dto.CreateDocumentTemplateWDTO
 import ru.alex3koval.docGenerator.domain.repository.document.dto.CreateDocumentWDTO
 import ru.alex3koval.docGenerator.domain.service.DocumentService
+import ru.alex3koval.docGenerator.domain.vo.DocumentFormat
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-class DocumentServiceImpl<T : Any, R>(
-    private val repository: DocumentRepository<T, R>,
+class DocumentServiceImpl<ID : Any, FILE_ID, TEMPLATE_ID : Any>(
+    private val documentRepository: DocumentRepository<ID, FILE_ID, TEMPLATE_ID>,
+    private val documentTemplateRepository: DocumentTemplateRepository<TEMPLATE_ID, FILE_ID>,
     private val objectMapper: ObjectMapper
-) : DocumentService<T, R> {
+) : DocumentService<ID, FILE_ID, TEMPLATE_ID>() {
+    override fun get(id: ID): Mono<DocumentEntity<ID, FILE_ID>> = documentRepository
+        .get(id)
+        .mapNotNull { rdto -> toEntity(rdto) }
+
+    override fun getTemplate(id: TEMPLATE_ID): Mono<DocumentTemplateEntity<TEMPLATE_ID, FILE_ID>> = documentTemplateRepository
+        .get(id)
+        .mapNotNull { rdto -> toEntity(rdto) }
+
     override fun createDocument(
-        fileId: R,
-        domainDto: BaseGeneratedDocDTO<R>
-    ): Mono<T> = repository.create(
+        templateId: TEMPLATE_ID,
+        fileId: FILE_ID,
+        domainDto: BaseGeneratedDocDTO,
+        format: DocumentFormat,
+        clazz: String
+    ): Mono<ID> = documentRepository.create(
+        toCreationWdto(
+            templateId = templateId,
+            fileId = fileId,
+            domainDto = domainDto,
+            format = format,
+            clazz = clazz
+        )
+    )
+
+    override fun createDocumentTemplate(
+        fileId: FILE_ID,
+        domainDto: BaseGeneratedDocDTO,
+        format: DocumentFormat,
+        clazz: String
+    ): Mono<TEMPLATE_ID> = documentTemplateRepository.create(
         toCreationWdto(
             fileId = fileId,
-            domainDto = domainDto
+            domainDto = domainDto,
+            format = format,
+            clazz = clazz
         )
     )
 
     private fun toCreationWdto(
-        fileId: R,
-        domainDto: BaseGeneratedDocDTO<R>
-    ): CreateDocumentWDTO<R> = run {
+        templateId: TEMPLATE_ID,
+        fileId: FILE_ID,
+        clazz: String,
+        domainDto: BaseGeneratedDocDTO,
+        format: DocumentFormat
+    ): CreateDocumentWDTO<FILE_ID, TEMPLATE_ID> = run {
         val now = LocalDateTime.now(ZoneOffset.UTC)
 
         CreateDocumentWDTO(
+            templateId = templateId,
             jsonModel = objectMapper.writeValueAsString(domainDto),
-            format = domainDto.format,
+            format = format,
+            clazz = clazz,
+            fileId = fileId,
+            createdAt = now,
+            updatedAt = now
+        )
+    }
+
+    private fun toCreationWdto(
+        fileId: FILE_ID,
+        clazz: String,
+        domainDto: BaseGeneratedDocDTO,
+        format: DocumentFormat
+    ): CreateDocumentTemplateWDTO<FILE_ID> = run {
+        val now = LocalDateTime.now(ZoneOffset.UTC)
+
+        CreateDocumentTemplateWDTO(
+            jsonModel = objectMapper.writeValueAsString(domainDto),
+            format = format,
+            clazz = clazz,
             fileId = fileId,
             createdAt = now,
             updatedAt = now
--- domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/service/DocumentService.kt
+++ domain/src/main/kotlin/ru/alex3koval/docGenerator/domain/service/DocumentService.kt
@@ -3,18 +3,47 @@ package ru.alex3koval.docGenerator.domain.service
 import reactor.core.publisher.Mono
 import ru.alex3koval.docGenerator.domain.contract.BaseGeneratedDocDTO
 import ru.alex3koval.docGenerator.domain.entity.DocumentEntity
+import ru.alex3koval.docGenerator.domain.entity.DocumentTemplateEntity
 import ru.alex3koval.docGenerator.domain.repository.document.dto.DocumentRDTO
+import ru.alex3koval.docGenerator.domain.repository.document.dto.DocumentTemplateRDTO
+import ru.alex3koval.docGenerator.domain.vo.DocumentFormat
 
-interface DocumentService<T, R> {
-    fun createDocument(
-        fileId: R,
-        domainDto: BaseGeneratedDocDTO<R>
-    ): Mono<T>
+abstract class DocumentService<ID, FILE_ID, TEMPLATE_ID : Any> {
+    abstract fun createDocument(
+        templateId: TEMPLATE_ID,
+        fileId: FILE_ID,
+        domainDto: BaseGeneratedDocDTO,
+        format: DocumentFormat,
+        clazz: String
+    ): Mono<ID>
 
-    fun DocumentRDTO<T, R>.toEntity(): DocumentEntity<T, R> = DocumentEntity(
-        id = id,
-        model = model,
-        format = format,
-        fileId = fileId
+    abstract fun createDocumentTemplate(
+        fileId: FILE_ID,
+        domainDto: BaseGeneratedDocDTO,
+        format: DocumentFormat,
+        clazz: String
+    ): Mono<TEMPLATE_ID>
+
+    abstract fun get(id: ID): Mono<DocumentEntity<ID, FILE_ID>>
+
+    abstract fun getTemplate(id: TEMPLATE_ID): Mono<DocumentTemplateEntity<TEMPLATE_ID, FILE_ID>>
+
+    protected fun toEntity(
+        rdto: DocumentRDTO<ID, FILE_ID, TEMPLATE_ID>
+    ): DocumentEntity<ID, FILE_ID> = DocumentEntity(
+        id = rdto.id,
+        domainDto = rdto.domainDto,
+        format = rdto.format,
+        fileId = rdto.fileId
+    )
+
+    protected fun toEntity(
+        rdto: DocumentTemplateRDTO<TEMPLATE_ID, FILE_ID>
+    ): DocumentTemplateEntity<TEMPLATE_ID, FILE_ID> = DocumentTemplateEntity(
+        id = rdto.id,
+        fileId = rdto.fileId,
+        domainDto = rdto.domainDto,
+        format = rdto.format,
+        clazz = rdto.clazz
     )
 }
